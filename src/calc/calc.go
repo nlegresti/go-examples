cefa2d57f6eff05cb0529a1a96ae4b92e9e9f952
nlegresti <nicolas.legresti@mercadolibre.com>
2020-05-22 12:42:58 -0300

    Add tests

--- src/calc/calc.go
+++ src/calc/calc.go
@@ -1,5 +1,24 @@
 package calc
 
+import "errors"
+
 func Sum(a, b int) int {
 	return a + b
+}
+
+func Div(a, b int) (int, error) {
+
+	if b == 0 {
+		return -1, errors.New("can't work with 0")
+	}
+
+	return a / b, nil
+}
+
+func Res(a, b int) int {
+	return a - b
+}
+
+func Multi(a, b int) int {
+	return a * b
 }
--- src/calc/calc_test.go
+++ src/calc/calc_test.go
@@ -16,3 +16,26 @@ func TestSumOk(t *testing.T) {
 	// THEN
 	assert.Equal(t, 9, result, fmt.Sprintf("Wrong sum!!"))
 }
+
+func TestDivOk(t *testing.T) {
+	// GIVEN
+	a := 6
+	b := 2
+	// WHEN
+	result, err := Div(a, b)
+	// THEN
+	assert.Equal(t, 3, result, fmt.Sprintf("Wrong div!!"))
+	assert.Nil(t, err, fmt.Sprintf("Wrong div - error not nil!!"))
+}
+
+func TestDivZero(t *testing.T) {
+	// GIVEN
+	a := 6
+	b := 0
+	// WHEN
+	result, err := Div(a, b)
+	// THEN
+	assert.Equal(t, -1, result, fmt.Sprintf("Wrong div!!"))
+	assert.NotNil(t, err, fmt.Sprintf("Wrong div - error nil!!"))
+	assert.Equal(t, "can't work with 0", err.Error(), fmt.Sprintf("Wrong error message!!"))
+}
